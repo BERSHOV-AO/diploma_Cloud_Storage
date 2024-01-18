ef2f9145b0398c2285f546f862592f65bf284031
Aleksey Bershov <bershov.aleksey@bk.ru>
2024-01-18 09:48:30 +0300

    Initial commit

---
+++ src/main/java/ru/netology/cloudstorage/DiplomaCloudStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.cloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaCloudStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaCloudStorageApplication.class, args);
+    }
+
+}
