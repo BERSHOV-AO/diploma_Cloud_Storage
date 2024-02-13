f4d7352ca30d2f8a33d5926b50aa78fcb16cf32f
Aleksey Bershov <bershov.aleksey@bk.ru>
2024-02-13 15:39:34 +0300

    Refactor pom.xml

--- src/main/java/ru/netology/cloudstorage/DiplomaCloudStorageApplication.java
+++ src/main/java/ru/netology/cloudstorage/DiplomaCloudStorageApplication.java
@@ -1,10 +1,7 @@
 package ru.netology.cloudstorage;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DiplomaCloudStorageApplication {
@@ -12,15 +9,4 @@ public class DiplomaCloudStorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiplomaCloudStorageApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(PasswordEncoder encoder) {
-        return args -> {
-            String password = "bershov2";
-            String encryptedPassword = encoder.encode(password);
-            System.out.println("Encrypted password: " + encryptedPassword);
-
-        };
-    }
-
 }
